86acc17b9f40fe08982ac4c8e8b99e015707ef31
YoungMoo Kim <rladuan612@gmail.com>
2026-01-13 17:33:15 +0900

    update weapon & shoot VFX

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int startingHealth = 3;
+
+    int currentHealth;
+
+    void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth < 1)
+        {
+            Destroy(this.gameObject);
+        }
+            
+    }
+ 
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,27 +1,46 @@
+using System.Runtime.CompilerServices;
 using StarterAssets;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     StarterAssetsInputs starterAssetsInputs;
+    [SerializeField] GameObject hitVFXPrefab;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] int damageAmount = 1;
+    [SerializeField] Animator animator;
 
+    const string SHOOT_STRING = "Shoot";
     void Awake()
     {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
     }
 
-    [SerializeField]
+
     void Update()
     {
-        if (starterAssetsInputs.shoot)
-        {
-            RaycastHit hit;
+        HandleShoot();
+    }
 
-            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
-            {
-                Debug.Log(hit.collider.name);
-                starterAssetsInputs.ShootInput(false);
-            }    
+    void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot) return;
+
+        muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        {
+            Instantiate(hitVFXPrefab, hit.point, quaternion.identity);
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth?.TakeDamage(damageAmount);
+            
         }
     }
-}
+}   
+
+    
